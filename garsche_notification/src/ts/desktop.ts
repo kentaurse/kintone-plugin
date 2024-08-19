5a7bbb650c1fb948c55182575b38f0befac35ad1
kentaurse <kentaurse0212@gmail.com>
2024-08-19 12:11:08 +0900

    build develop env

---
+++ garsche_notification/src/ts/desktop.ts
@@ -0,0 +1,9 @@
+function main(PLUGIN_ID: string) {
+  ("use strict");
+
+  kintone.events.on("", async function (event) {
+    return //return event to change kintone
+  });
+}
+
+main(kintone.$PLUGIN_ID);
---
+++ garsche_notification/src/ts/types/kintone.d.ts
@@ -0,0 +1,25 @@
+declare namespace kintone {
+  const $PLUGIN_ID: string;
+
+  namespace app {
+    function getId(): number;
+    namespace record {
+      function get(): Promise<{ record: Record<string, any> }>;
+      function set(data: { record: Record<string, any> }): Promise<void>;
+      function setFieldShown(fieldCode: string, isVisible: boolean): void;
+    }
+    function getHeaderMenuSpaceElement(): HTMLElement;
+  }
+
+  namespace events {
+    function on(event: string | string[], handler: (event: any) => any): void;
+  }
+
+  namespace plugin {
+    namespace app {
+      function getConfig(pluginId: string): Record<string, any>;
+    }
+  }
+
+  function api(url: string, method: "GET" | "POST", body: any, success: (response: any) => void, failure: (error: any) => void): void;
+}
