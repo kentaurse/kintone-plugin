398d428c8e4a739476c9ff58b157a497fc934591
dd <dd@gmail.com>
2024-09-19 11:09:15 +0900

    1

--- garsche_notification/dist/src/ts/desktop.js
+++ garsche_notification/dist/src/ts/desktop.js
@@ -1,15 +1,29 @@
 "use strict";
-const axios = require('axios');
+const axios = require("axios");
 function main(PLUGIN_ID) {
     ("use strict");
     const onLoadEvent = "app.record.index.show";
     const initialEvent = ["app.record.create.show", "app.record.edit.show"];
     kintone.events.on(initialEvent, async function (event) {
         const record = event.record;
-        record["title"].value = "title";
         record["createdAt"].disabled = true;
-        record["status"].value = "ドラフト";
         record["status"].disabled = true;
+        let startDate = record["startDate"].value || "";
+        const duringDate = record["duringDate"].value || null;
+        let endDate = record["endDate"].value || "";
+        if (startDate !== "") {
+            const newDate = new Date(startDate);
+            newDate.setDate(newDate.getDate() + duringDate);
+            const calculatedEndDateStr = newDate.toISOString().split("T")[0];
+            endDate = calculatedEndDateStr;
+        }
+        const today = new Date();
+        if (today > new Date(endDate)) {
+            record["status"].value = "ドラフト";
+        }
+        else {
+            record["status"].value = "公開済み";
+        }
         return event;
     });
     const showEvent = initialEvent.concat(["app.record.create.change.startDate", "app.record.edit.change.startDate"]);
@@ -43,62 +57,68 @@ function main(PLUGIN_ID) {
     const buttonEvent = initialEvent.concat(["app.record.detail.show"]);
     kintone.events.on(buttonEvent, async (event) => {
         let record = event.record;
-        const keepButton = document.createElement('button');
-        keepButton.id = 'keepButton';
-        keepButton.innerText = '保存';
-        keepButton.style.marginLeft = '8px';
+        const keepButton = document.createElement("button");
+        keepButton.id = "keepButton";
+        keepButton.innerText = "保存";
+        keepButton.style.marginLeft = "8px";
         // const keepSpaceField = kintone.app.record.getSpaceElement('keep');
         // keepSpaceField.appendChild(keepButton);
-        const generateButton = document.createElement('button');
-        generateButton.id = 'generateButton';
-        generateButton.innerText = '生成';
-        generateButton.style.marginLeft = '8px';
+        const generateButton = document.createElement("button");
+        generateButton.id = "generateButton";
+        generateButton.innerText = "生成";
+        generateButton.style.marginLeft = "8px";
         generateButton.onclick = async function () {
             const updatedRecord = (await kintone.app.record.get()).record;
             record = updatedRecord;
-            const startDate = record["startDate"].value || '';
+            let startDate = record["startDate"].value || "";
             const duringDate = record["duringDate"].value || null;
-            const newDate = new Date(startDate);
-            newDate.setDate(newDate.getDate() + duringDate);
-            const calculatedEndDateStr = newDate.toISOString().split('T')[0];
-            const endDate = record["endDate"].value || calculatedEndDateStr || '';
-            const content = record["content"].value || '';
-            const works = record["works"].value || '';
-            const system = record["system"].value || '';
-            record["notificationContent"].value = `【メンテナンス日時】\r\n${startDate} ～ ${endDate}\r\n\r\n${content}\r\n\r\n【ユーザー様のご対応】\r\n${works}\r\n\r\n【システム停止】\r\n${system}\r\n`;
+            let endDate = record["endDate"].value || "";
+            let displayDate = `終了日：${endDate}`;
+            if (startDate !== "") {
+                const newDate = new Date(startDate);
+                newDate.setDate(newDate.getDate() + duringDate);
+                const calculatedEndDateStr = newDate.toISOString().split("T")[0];
+                endDate = calculatedEndDateStr;
+                displayDate = `${startDate} ～ ${endDate}`;
+            }
+            const content = record["content"].value || "";
+            const works = record["works"].value || "";
+            const system = record["system"].value || "";
+            record["notificationContent"].value =
+                `【メンテナンス日時】\r\n${displayDate}\r\n\r\n${content}\r\n\r\n【ユーザー様のご対応】\r\n${works}\r\n\r\n【システム停止】\r\n${system}\r\n`;
             setTimeout(() => {
                 kintone.app.record.set({ record });
             }, 0);
         };
-        const generateSpaceField = kintone.app.record.getSpaceElement('generate');
+        const generateSpaceField = kintone.app.record.getSpaceElement("generate");
         generateSpaceField.appendChild(generateButton);
-        const shareButton = document.createElement('button');
-        shareButton.id = 'shareButton';
-        shareButton.innerText = '公開';
-        shareButton.style.marginLeft = '8px';
+        const shareButton = document.createElement("button");
+        shareButton.id = "shareButton";
+        shareButton.innerText = "公開";
+        shareButton.style.marginLeft = "8px";
         shareButton.onclick = async () => {
             const headers = {
                 "Content-Type": "application/json",
                 "x-api-key": "dfSe2W4BxG8EtfH3eqVIL6001b2uD6R01FwarSAy",
-                "Access-Control-Allow-Origin": ""
+                "Access-Control-Allow-Origin": "",
             };
             const data = {
                 notification: {
                     created_at: Date.now(),
                     exp_date: Date.now() + 30 * 24 * 60 * 60 * 1000,
-                    title: 'Your Notification Title',
-                    body: 'Content of the notification'
-                }
+                    title: record["title"].value,
+                    body: record["notificationContent"].value,
+                },
             };
             try {
-                const response = await axios.put('https://api.dev.garsche.net/db/notification', data, { headers });
-                console.log('Success:', response.data);
+                const response = await axios.put("https://api.dev.garsche.net/db/notification", data, { headers });
+                record["status"].value = "公開済み";
             }
             catch (error) {
-                console.error('Error:', error.response ? error.response.data : error.message);
+                console.error("Error:", error.response ? error.response.data : error.message);
             }
         };
-        const shareSpaceField = kintone.app.record.getSpaceElement('share');
+        const shareSpaceField = kintone.app.record.getSpaceElement("share");
         shareSpaceField.appendChild(shareButton);
         return event;
     });
